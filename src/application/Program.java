fbe16898e422e4ab216bdc4b54f134684aa12905
Marcello Venturini <75899197+marcelloventurini@users.noreply.github.com>
2022-08-11 10:37:38 -0300

    Add files via upload

---
+++ src/application/Program.java
@@ -0,0 +1,32 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    
+    System.out.print("Enter the number quantity: ");
+    int n = scanner.nextInt();
+    int[] vect = new int[n];
+
+    for (int i = 0; i < vect.length; i++) {
+      System.out.print("Enter a number: ");
+      vect[i] = scanner.nextInt();
+    }
+
+    System.out.println("Pairs: ");
+
+    int pairs = 0;
+    for (int i = 0; i < vect.length; i++) {
+      if (vect[i] % 2 == 0) {
+        System.out.printf("%d ", vect[i]);
+        pairs++;
+      }
+    }
+
+    System.out.println("\nPairs quantity: " + pairs);
+
+    scanner.close();
+  }
+}
